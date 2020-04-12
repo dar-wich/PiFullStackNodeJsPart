4f7f271d6017d3512103796b4cc93df2cd66bc1a
Lamari Mohamed Amine <mohamedamine.lamari@esprit.tn>
2020-04-12 19:12:39 +0100

    hell o

--- routes/preprocessing.js
+++ routes/preprocessing.js
@@ -5,39 +5,53 @@ var d3 = require("d3");
 var _ = require("lodash");
 
 const Data = require('../models/Data');
+const translate = require('@k3rn31p4nic/google-translate-api');
 
-router.get('/', function(req, res, next) {
-    
-    fs.readFile("public/quoted.csv", "utf8", function(error, data) {
+router.get('/', function (req, res, next) {
+
+    fs.readFile("public/quoted.csv", "utf8", function (error, data) {
         data = d3.csvParse(data);
-        
-        for(let a=0;a<data.length;a++){
-            
-            var myData = new Data(
-         {id:data[a].id,
-         TweetId:data[a].TweetId,
-         userName:data[a].UserName,
-         Date:data[a].date,
-         Retweet:data[a].retweets,
-         Text:data[a].text,
-         Geoloc:data[a].geoloc,
-         textTranslated:'default'},
+        var finale = [];
+        var myData = new Data();
+        for (let a = 0; a < data.length; a++) {
 
-         );
-         
-     myData.save()
-       .then(item => {
-        console.log("data saved to database");
-       })
-       .catch(err => {
-        console.log(err)
-       }); 
-       
-   }
-        res.send(JSON.stringify(data));
-      });
-  });
-  
-  
-  
-  module.exports = router;
+            console.log("============================================================================"+data[a].text);
+            translate(data[a].text, { to: 'en' }).then(tr => {
+                console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"+tr);
+                myData = new Data(
+                    {
+                        id: data[a].id,
+                        TweetId: data[a].TweetId,
+                        userName: data[a].UserName,
+                        Date: data[a].date,
+                        Retweet: data[a].retweets,
+                        Text: data[a].text,
+                        Geoloc: data[a].geoloc,
+                        textTranslated: tr
+                    },
+
+                );
+                
+            }).catch(err => {
+                console.error(err);
+            });
+            myData.save()
+                    .then(item => {
+                        console.log("data saved to database");
+                    })
+                    .catch(err => {
+                        console.log(err)
+                    });
+
+            finale.push(myData);
+
+
+
+        }
+        res.send(JSON.stringify(finale));
+    });
+});
+
+
+
+module.exports = router;
