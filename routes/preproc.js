f467d755ee8edb25e082966123c8476a6bfed2ce
Lamari Mohamed Amine <mohamedamine.lamari@esprit.tn>
2020-04-20 20:00:48 +0100

    hell'o

--- routes/preproc.js
+++ routes/preproc.js
@@ -18,28 +18,30 @@ async function Do() {
     tab.forEach((element, j) => {
       setTimeout(() => {
         const text = translate.translate(element.text, { to: 'en' }, function (err, res) {
-          if (res != undefined) {
-            i++;
-            console.log(res.text + " " + i)
-            var myData = new Data(
-              {
-                id: element.id,
-                TweetId: element.TweetId,
-                userName: element.UserName,
-                Date: element.date,
-                Retweet: element.retweets,
-                Text: element.text,
-                Geoloc: element.geoloc,
-                textTranslated: res.text + ""
-              },
-            );
-            myData.save()
-              .then(item => {
-                console.log("data " + i + " saved to database");
-              })
-              .catch(err => {
-                console.log("error")
-              });
+          if ((res != undefined)&&(res.text!=undefined)) {
+            
+              i++;
+              console.log(res.text + " " + i)
+              var myData = new Data(
+                {
+                  id: element.id,
+                  TweetId: element.TweetId,
+                  userName: element.UserName,
+                  Date: element.date,
+                  Retweet: element.retweets,
+                  Text: element.text,
+                  Geoloc: element.geoloc,
+                  textTranslated: res.text + ""
+                },
+              );
+              myData.save()
+                .then(item => {
+                  console.log("data " + i + " saved to database");
+                })
+                .catch(err => {
+                  console.log("error")
+                });
+            
           }
         })
       }, j * 500);
@@ -60,73 +62,70 @@ router.get('/', function (req, res, next) {
 
 });
 
-async function sentiment(sentence){
-	const { SentimentAnalyzer, PorterStemmer } = natural;
-    const analyzer = new SentimentAnalyzer('English', PorterStemmer, 'afinn');
-    const analysis = analyzer.getSentiment(sentence);
-	return analysis;
+async function sentiment(sentence) {
+  const { SentimentAnalyzer, PorterStemmer } = natural;
+  const analyzer = new SentimentAnalyzer('English', PorterStemmer, 'afinn');
+  const analysis = analyzer.getSentiment(sentence);
+  return analysis;
 }
 
 /*const analysis = (sentence)=>{
   return analyzer.getSentiment(sentence)
 }*/
 
- router.get('/testAnalysis', (req,res,next)=>{
-   let arrayExample = ["I","and","love", "cherries"]
-   let sen = sentiment(arrayExample);
-   res.send(JSON.stringify(sentiment(arrayExample)));
- })
- 
+router.get('/testAnalysis', (req, res, next) => {
+  let arrayExample = ["I", "and", "love", "cherries"]
+  let sen = sentiment(arrayExample);
+  res.send(JSON.stringify(sentiment(arrayExample)));
+})
+
 async function prePro() {
   let result = [];
   Data.find({}, function (err, datas) {
     datas.forEach(x => {
-      if((x.textTranslated!="undefined")&&(x.textTranslated!="")){
-      const lexedReview = aposToLexForm(x.textTranslated);
+      if ((x.textTranslated != "undefined") && (x.textTranslated != "")) {
+        const lexedReview = aposToLexForm(x.textTranslated);
 
-      const casedReview = lexedReview.toLowerCase();
+        const casedReview = lexedReview.toLowerCase();
 
-      let alphaOnlyReview = casedReview.replace('""', '');
-      alphaOnlyReview = alphaOnlyReview.replace('RT ', '');
-      alphaOnlyReview = alphaOnlyReview.replace(/@[A-Za-z0-9]+/gm, '');
-      alphaOnlyReview = alphaOnlyReview.replace(/^(\s*#\w+\s*)+$/gm, "")
-      alphaOnlyReview = alphaOnlyReview.replace(/(?:https?|ftp):\/\/[\n\S]+/gm, '');
-      alphaOnlyReview = alphaOnlyReview.replace(/[^a-zA-Z\s]+/gm, '');
+        let alphaOnlyReview = casedReview.replace('""', '');
+        alphaOnlyReview = alphaOnlyReview.replace('RT ', '');
+        alphaOnlyReview = alphaOnlyReview.replace(/@[A-Za-z0-9]+/gm, '');
+        alphaOnlyReview = alphaOnlyReview.replace(/^(\s*#\w+\s*)+$/gm, "")
+        alphaOnlyReview = alphaOnlyReview.replace(/(?:https?|ftp):\/\/[\n\S]+/gm, '');
+        alphaOnlyReview = alphaOnlyReview.replace(/[^a-zA-Z\s]+/gm, '');
 
-      const { WordTokenizer } = natural;
-      const tokenizer = new WordTokenizer();
-      const tokenizedReview = tokenizer.tokenize(alphaOnlyReview);
+        const { WordTokenizer } = natural;
+        const tokenizer = new WordTokenizer();
+        const tokenizedReview = tokenizer.tokenize(alphaOnlyReview);
 
-      const filteredReview = SW.removeStopwords(tokenizedReview);
-      let sen =  sentiment(filteredReview);
-	  console.log(sentiment);
-      x.sentiment = sen;
-      x.save()
-      result.push(filteredReview);
+        const filteredReview = SW.removeStopwords(tokenizedReview);
+        let sen = sentiment(filteredReview);
+        console.log(sen);
+        x.sentiment = sen;
+        x.save();
+        result.push(sen);
       }
     });
-  }); 
+  });
   return result;
 }
 
 router.get('/analysis', async function (req, res, next) {
   let result = await prePro();
-  setTimeout(()=>{
+  setTimeout(() => {
     res.send(JSON.stringify(result));
-  },1000);
-  
-  
+  }, 1000);
+
+
 });
 
-router.get('/removeAll',async function (req, res, next) {
+router.get('/removeAll', async function (req, res, next) {
   Data.find({}, function (err, datas) {
-    datas.forEach(x => {
-      x.remove();
-	  
-    });
+    
     res.send(JSON.stringify(datas));
   });
-  
+
 
 });
 
